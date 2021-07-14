64bbc26316f905bba48c6ea9899f6cd4519a6449
Gabe=DiAntonio <gbbdiantonio@gmail.com>
2021-07-14 10:16:41 -0500

    absorb_boundry

--- src/Diffusion.jl
+++ src/Diffusion.jl
@@ -1,9 +1,14 @@
 module Diffusion
+using StaticArrays 
 
 export absorb_boundry
 
 function absorb_boundry(pos,r)
-    return pos + r
+    if(((pos[1]^2)+(pos[2]^2))^(1/2) < r)
+    return pos
+    else
+    return nothing
+    end
 end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,11 @@
 using Diffusion
 using Test
+using StaticArrays
 
 @testset "Diffusion.jl" begin
     # Write your tests here.
-    @test absorb_boundry(1,2) == 3
+    @test absorb_boundry(SA[3,4],6) == SA[3,4]
+    @test absorb_boundry(SA[3,4],5) === nothing
+    @test absorb_boundry(SA[3,4],4) === nothing 
 end
 
